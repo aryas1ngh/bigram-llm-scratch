4d677819856eaec76040dea6bbba7c5e2de8c28a
arya <97499206+aryas1ngh@users.noreply.github.com>
2024-01-27 15:03:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from app_model import BGM
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+
+# can be changed according to 'vocab_size' used for training
 vocab_size = 104
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -22,8 +24,13 @@ itos = {i:ch for i,ch in enumerate(chars)}
 encode=lambda s : [stoi[c] for c in s]
 decode= lambda s : ''.join([itos[i] for i in s])
 
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    ''' 
+        index page which takes 'max_new_tokens' as input
+    '''
     if request.method == 'POST':
 
         max_new_tokens = int(request.form['max_new_tokens'])
@@ -39,7 +46,9 @@ def index():
 
 @app.route('/result')
 def result():
-
+    ''' 
+        result page which displays generated text.
+    '''
     generated_text = session.pop('generated_text', '')
     max_new_tokens = session.pop('max_new_tokens', 0)
 
@@ -51,5 +60,6 @@ def generate_text(model, context, max_new_tokens):
     generated_text = ''.join([itos[i] for i in generated_tokens])
     return generated_text
 
+# run app
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
